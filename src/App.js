ae5a461cac014a9c4d601b9db8fa076693a8dc6d
skberg <skb@markvn10.com>
2022-09-18 14:42:24 +0200

    datafetch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
+
 import React from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
-import Layout from './comp/StyleComp/Layout';
+
 
 function App() {
 
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState();
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://storage.googleapis.com/aller-structure-task/test_data.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+          console.log(result);
+         
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+
+ console.log(items)
+
+
+  }
   return (
-  <>
-    <Layout>
- 
-      
-        
+    <>
+             
+     
+              <div>
+        {items.map((item, i )=> (
+          <ul key={i}>
+           
+            <li>{item[i].columns[i].title} 
+            <br></br>
+            {item[i].columns[i].type}</li>
+          </ul>
+        ))}
+      </div>
 
-            <div>https://storage.googleapis.com/aller-structure-task/test_data.json</div>
-
-  
 
 
-  </Layout>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import Layout from './comp/StyleComp/Layout';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout>
-    <App />
-    </Layout>
+ 
+        <App />
+ 
   </React.StrictMode>
 );
 
